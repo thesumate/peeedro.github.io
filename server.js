27dec8c10ee5077fefe9f3217f234a4f1e767b65
thesumate <123641818+thesumate@users.noreply.github.com>
2024-04-21 12:07:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+let receivedValue = '';
+
+app.post('/empfangen', (req, res) => {
+  receivedValue = req.body.data;
+  console.log('Empfangener Wert:', receivedValue);
+  res.send('Wert empfangen und gespeichert.');
+});
+
+app.get('/wert', (req, res) => {
+  res.send(receivedValue);
+});
+
+app.listen(port, () => {
+  console.log(`Server läuft auf http://localhost:${port}`);
+});
